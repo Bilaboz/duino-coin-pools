fabaf3e5e1ef53eba1144d92e6e0a32370cc9482
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-09-17 08:04:22 -0700

    Pools update

--- src/connectionHandler.js
+++ src/connectionHandler.js
@@ -1,85 +1,93 @@
-const fs = require("fs");
+/* Duino-Coin Connection handler
+For documention about these functions see
+https://github.com/revoxhere/duino-coin/blob/useful-tools
+2019-2021 Duino-Coin community */
 
-const mining = require("./mining");
-const { maxWorkers, motd, serverVersion } = require("../config/config.json")
-const bans = require("../config/bans.json");
+const fs = require('fs');
+const axios = require('axios');
+const mining = require('./mining');
+const {
+    maxWorkers,
+    motd,
+    serverVersion
+} = require('../config/config.json');
+let bans = require('../config/bans.json');
 
 const handle = (conn) => {
-    conn.setEncoding("utf8");
-    // generate a unique id for the connection
     conn.id = Math.random().toString(36).substr(2, 9);
-    //console.log(`New incoming connection: ${conn.remoteAddress}#${conn.id}`);
-
     try {
+        conn.setTimeout(10000);
+        conn.setNoDelay(true);
+        conn.setEncoding('utf8');
         conn.write(serverVersion);
-        conn.setTimeout(90000);
-    }
-    catch(err) {}
+    } catch (err) {}
 
-    conn.on("close", () => {
-        //console.log(`${conn.remoteAddress}#${conn.id} close disconnected`);
+    conn.on('close', () => {
         try {
             delete mining.stats.minersStats[conn.id];
-        } catch {}
+        } catch (err) {}
         try {
             mining.stats.workers[conn.remoteAddress] -= 1;
-            if (mining.stats.workers[conn.remoteAddress] <= 0) delete mining.stats.workers[conn.remoteAddress];
-        } catch {}
+            if (mining.stats.workers[conn.remoteAddress] <= 0)
+                delete mining.stats.workers[conn.remoteAddress];
+        } catch (err) {}
         try {
             mining.stats.usrWorkers[conn.username] -= 1;
-            if (mining.stats.usrWorkers[conn.username] <= 0) delete mining.stats.usrWorkers[conn.username];
-        } catch {}
+            if (mining.stats.usrWorkers[conn.username] <= 0)
+                delete mining.stats.usrWorkers[conn.username];
+        } catch (err) {}
     })
 
-    conn.on("error", (err) => {
-        if (err.code !== "ECONNRESET") {
-            //console.log(`Socket error in connection handler: ${err}`);
-        }
+    conn.on('error', (err) => {
+        if (err.code !== 'ECONNRESET') {}
     })
-    
-    conn.on("timeout", (err) => {
-        //console.log(`${conn.remoteAddress}#${conn.id} timeout`);
+
+    conn.on('timeout', (err) => {
         conn.destroy();
     })
 
-    conn.on("data", function mainListener (data) {
-        data = data.trim();
-        data = data.split(",");
+    conn.on('data', function mainListener(data) {
+        data = data.trim().split(',');
 
-        if (bans.bannedIPs.includes(conn.remoteAddress)) { // check if the ip is banned
+        if (data.length > 5) {
+            conn.write('101 Switching Protocol');
             return conn.destroy();
         }
 
-        if (data[0] === "JOB") {
-            if (!data[1]) {  // check if username was provided
-                conn.write("NO,Not enough data");
+        if (!conn.remoteAddress) {
+            conn.write('418 I\'m a teapot');
+            return conn.destroy();
+        }
+
+        if (bans.bannedIPs.includes(conn.remoteAddress)) {
+            conn.write('204 No Content');
+            return conn.destroy();
+        }
+
+        if (bans.bannedUsernames.includes(data[1])) {
+            conn.write('301 Moved Permanently');
+            bans.bannedIPs.push(conn.remoteAddress);
+            fs.writeFileSync('../config/bans.json', JSON.stringify(bans, null, 0));
+            return conn.destroy();
+        }
+
+        if (data[0] === 'JOB') {
+            if (!data[1]) {
+                conn.write('NO,Not enough data');
                 return conn.destroy();
             }
+            mining.miningHandler(conn, data, mainListener, false, false);
 
-            if (bans.bannedUsernames.includes(data[1])) {
-                bans.bannedIPs.push(conn.remoteAddress); // ban the ip
-                fs.writeFileSync("./config/bans.json", JSON.stringify(bans, null, 4));
+        } else if (data[0] === 'JOBXX') {
+            if (!data[1]) {
+                conn.write('NO,Not enough data');
                 return conn.destroy();
             }
+            mining.miningHandler(conn, data, mainListener, true, false);
 
-            mining.miningHandler(conn, data, mainListener, false);
-        } else if (data[0] === "JOBXX") {
-            if (!data[1]) {  // check if username was provided
-                conn.write("NO,Not enough data");
-                return conn.destroy();
-            }
-
-            if (bans.bannedUsernames.includes(data[1])) {
-                bans.bannedIPs.push(conn.remoteAddress); // ban the ip
-                fs.writeFileSync("./config/bans.json", JSON.stringify(bans, null, 4));
-                return conn.destroy();
-            }
-
-            mining.miningHandler(conn, data, mainListener, true);
-        } else if (data[0] === "MOTD") {
+        } else if (data[0] === 'MOTD') {
             let finalMOTD = motd;
             finalMOTD += `\nPool worker limit: ${maxWorkers}`
-
             conn.write(finalMOTD);
         }
     })
