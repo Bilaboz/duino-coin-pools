3c797be38342ac44a2459896d604283af33203f4
Bilaboz <44091093+Bilaboz@users.noreply.github.com>
2022-01-17 21:10:54 +0100

    Update logging.js

--- src/logging.js
+++ src/logging.js
@@ -1,25 +1,25 @@
 const chalk = require('chalk');
-const info = chalk.blue;
-const error = chalk.bold.red;
-const success = chalk.green;
-const warning = chalk.hex('#FFA500');
+const blue = chalk.blue;
+const red = chalk.bold.red;
+const green = chalk.green;
+const orange = chalk.hex('#FFA500');
 
 const { poolName } = require("../config/config.json");
 
 const info = (msg) => {
-	console.log(`${poolName}: ${new Date().toLocaleString()} ` + info(msg));
+	console.log(`${poolName}: ${new Date().toLocaleString()} ` + blue(msg));
 }
 
 const success = (msg) => {
-	console.log(`${poolName}: ${new Date().toLocaleString()} ` + success(msg));
+	console.log(`${poolName}: ${new Date().toLocaleString()} ` + green(msg));
 }
 
 const warning = (msg) => {
-	console.log(`${poolName}: ${new Date().toLocaleString()} ` + warning(msg));
+	console.log(`${poolName}: ${new Date().toLocaleString()} ` + orange(msg));
 }
 
 const error = (msg) => {
-	console.log(`${poolName}: ${new Date().toLocaleString()} ` + error(msg));
+	console.log(`${poolName}: ${new Date().toLocaleString()} ` + red(msg));
 }
 
 module.exports = {
