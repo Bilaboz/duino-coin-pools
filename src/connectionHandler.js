5d6a24e6fb18105c26f78f0a5c9bef3cd349b898
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2022-04-09 16:16:16 +0200

    Update connectionHandler.js

--- src/connectionHandler.js
+++ src/connectionHandler.js
@@ -1,7 +1,7 @@
 /* Duino-Coin Connection handler
 For documention about these functions see
 https://github.com/revoxhere/duino-coin/blob/useful-tools
-2019-2021 Duino-Coin community */
+2019-2022 Duino-Coin community */
 
 const fs = require('fs');
 const crypto = require('crypto');
